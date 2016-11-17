2c74fa721d342f276e351d26242c1b229fc1e767
Jen <contact.vandepool@gmail.com>
2016-11-17 02:45:36 +0000

    Create, edit & delete events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,50 @@
 class EventsController < ApplicationController
     
+    def home
+    end
+    
     def index
-        @events = Event.all
+        # @events = Event.all
+        
+        @events = Event.upcoming
     end
     
     def show
         @event = Event.find(params[:id])
     end
+    
+    def edit
+        @event = Event.find(params[:id])
+    end
+    
+    def update
+        @event = Event.find(params[:id])
+        @event.update(event_params)
+        redirect_to event_path(@event)
+    end
+    
+    def new
+        @event = Event.new
+    end
+    
+    def create
+        @event = Event.new(event_params)
+        @event.save
+        redirect_to event_path(@event)
+    end
+    
+    def destroy 
+        @event = Event.find(params[:id])
+        @event.destroy
+        redirect_to events_url
+    end
+    
+    private
+    
+        def find_event
+        end
+        
+        def event_params
+             params.require(:event).permit(:name, :description, :location, :price, :starts_at)
+        end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,20 @@
 class Event < ApplicationRecord
-    
+    # Define price as free
     def free?
         price.blank? || price.zero?
+    end
+    
+    # Display upcoming events
+    def self.upcoming
+        where('starts_at >= ?', Time.now).order('starts_at')
+    end 
+    
+    # Display upcoming events, limit to 10 results
+    def self.upcoming10
+        where('starts_at >= ?', Time.now).order('starts_at').limit(10)
+    end 
+    
+    # Display most registered events
+    def self.popular
     end
 end
