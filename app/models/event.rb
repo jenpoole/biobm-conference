9a219e1cb624fe1d557e4824418568463f4a4630
Jen <contact.vandepool@gmail.com>
2016-11-17 02:59:05 +0000

    list upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,11 +9,6 @@ class Event < ApplicationRecord
         where('starts_at >= ?', Time.now).order('starts_at')
     end 
     
-    # Display upcoming events, limit to 10 results
-    def self.upcoming10
-        where('starts_at >= ?', Time.now).order('starts_at').limit(10)
-    end 
-    
     # Display most registered events
     def self.popular
     end
