03822d6a4472a7d12216ea01f958a301d3a5cb5e
Jen <contact.vandepool@gmail.com>
2016-11-17 00:14:41 +0000

    Events description, date & price

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,12 @@
 module EventsHelper
+    
+    # Format Event pricing
+    def format_price(event)
+        if event.free?
+         "Free"   
+        else
+            number_to_currency(event.price)
+        end    
+    end
+    
 end
