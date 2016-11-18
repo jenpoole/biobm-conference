2ec4f399f282825b0c073d4e9607ed2778d4c37f
Jen <contact.vandepool@gmail.com>
2016-11-18 21:20:13 +0000

    turbolinks load fix for datetimepicker

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,3 +1,10 @@
 // $(document).ready(function() {
 //     $('.datetimepicker').datetimepicker();
 // });
+
+// Fix for JS & Turbolinks on initial load
+$(document).on('turbolinks:load', function() {
+
+  $('.datetimepicker').datetimepicker();
+
+});
