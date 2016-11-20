6ca47bc6dc10601bac4e944a4d72f184168a0f08
Jen <contact.vandepool@gmail.com>
2016-11-20 01:14:07 +0000

    Update user registration form (join email list option)

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -3,7 +3,7 @@ class UserRegistrationsController < Devise::RegistrationsController
     private
     
         def sign_up_params
-            params.require(:user).permit(:first_name, :last_name, :country, :company, :email, :password, :password_confirmation)
+            params.require(:user).permit(:first_name, :last_name, :country, :company, :email, :password, :password_confirmation, :join_email_list)
         end
             
         def account_update_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+         
+  JOIN_EMAIL_LIST_OPTIONS = [
+    'Yes',
+    'No'
+  ]       
 end
