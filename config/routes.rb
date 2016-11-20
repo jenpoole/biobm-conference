fa620ef79e40a53c97dbe346a6af482c14668f93
Jen <contact.vandepool@gmail.com>
2016-11-20 02:19:08 +0000

    Add user profile show & edit pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   
   # Allow custom fields for devise users
   devise_for :users, :controllers => { registrations: 'user_registrations' }
-
+  
+  # user profile page
+  resources :users, only: [:show, :edit, :update]
+  
   resources :events do 
       resources :registrations
   end
