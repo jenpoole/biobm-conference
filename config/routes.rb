a7f1d4767a7930e786b6491cd1595aab90c4ac38
Jen <contact.vandepool@gmail.com>
2016-11-22 01:55:22 +0000

    format sponsor logos

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ Rails.application.routes.draw do
   # Allow custom fields for devise user registration
   devise_for :users, :controllers => { registrations: 'user_registrations' }
   
-  #route to user profile page
+  # route to user profile page
   resources :users, only: [:show]
   
   # route to manage user's events
   get "/users/:id/manage_events" => "users#manage_events", :as => :manage_events
   
-  resources :events do 
+  resources :events do  
       resources :registrations
   end
 end
